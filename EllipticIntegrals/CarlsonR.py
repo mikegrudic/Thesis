23e385cfb304395c6041fd6f10a902a9ab7d954b
Michael Grudich <mgrudich@mun.ca>
2013-10-26 18:48:58 -0230

    Boost Carlson integrals, function to compute the Terrible Integral

--- EllipticIntegrals/CarlsonR.py
+++ EllipticIntegrals/CarlsonR.py
@@ -1,18 +1,12 @@
 import numpy as np
+import math
 from scipy import weave
 
-def RF(x,y,z):
-    """ RF
-    Calculates Carlson's elliptic integral of the first kind RF(x,y,z)
-    
-    C++ code comes directly from Numerical Recipes 3e by Press et al., section 6.12
-    """
-
-    code = """
+RFcode = """
     using namespace std;
 
-    const double ERRTOL = 0.0025, THIRD = 1.0/3.0, C1 = 1.0/24.0, C2=0.1, C3 = 3.0/44.0, C4 = 1.0/14.0;
-    const double TINY = 5.0*DBL_MIN, BIG= 0.2*DBL_MAX;
+    static const double ERRTOL = 0.0025, THIRD = 1.0/3.0, C1 = 1.0/24.0, C2=0.1, C3 = 3.0/44.0, C4 = 1.0/14.0;
+    static const double TINY = 5.0*DBL_MIN, BIG= 0.2*DBL_MAX;
     double alamb, ave, delx, dely, delz, e2, e3, sqrtx, sqrty, sqrtz, xt, yt, zt;
     if (min(min(x,y),z) < 0.0 || min(min(x+y,x+z),y+z) < TINY || max(max(x,y),z) > BIG) throw("Invalid arguments in rf");
     xt = x;
@@ -37,4 +31,67 @@ def RF(x,y,z):
 
     return_val = (1.0 + (C1*e2 - C2 - C3*e3)*e2 + C4*e3)/sqrt(ave);
     """
-    return weave.inline(code,['x','y','z'],headers=["<float.h>","<algorithm>"])
+
+RJcode = """
+using namespace std;
+
+static const double ERRTOL = 0.0015, C1 = 3.0/4.0, C2 = 1.0/3.0, C3 = 3.0/22.0, C4 = 3.0/26.0, C5 = 0.75*C3, C6 = 1.5*C4, C7 = 0.5*C2, C8 = C3 + C3;
+static const double TINY = pow(5.0*DBL_MIN, 1./3.) , BIG= 0.3*pow(0.2*DBL_MAX, 1./3.);
+
+double a, alamb, alpha, ans, ave, b, beta, delp, delx, dely, delz, ea, eb, ec, ed, ee, fac, pt, rcx, rho, sqrtx, sqrty, sum, tau, xt, yt, zt;
+
+if (min(min(x,y),z) < 0.0 || min(min(x+y,x+z),min(y+z,abs(p))) < TINY || max(max(x,y),max(z,abs(p))) > BIG) throw("Invalid arguments in RJ");
+
+
+
+"""
+
+
+def RF(x,y,z):
+    """ RF
+    Calculates Carlson's elliptic integral of the first kind RF(x,y,z)
+    
+    C++ code comes directly from Numerical Recipes 3e by Press et al., section 6.12
+    """
+
+    return weave.inline(RFcode,['x','y','z'],headers=["<float.h>","<algorithm>"])
+
+def BoostRF(x,y,z):
+    RFcode = """
+    return_val = boost::math::ellint_rf(x, y, z);
+    """
+    return weave.inline(RFcode, ['x','y','z'], headers = ["<boost/math/special_functions/ellint_rf.hpp>"])
+
+def BoostRJ(x,y,z,p):
+    RJcode = """
+    return_val = boost::math::ellint_rj(x, y, z, p);
+    """
+    return weave.inline(RJcode, ['x','y','z','p'], headers = ["<boost/math/special_functions/ellint_rj.hpp>"])
+
+def BoostRC(x,y):
+    RCcode = """
+    return_val = boost::math::ellint_rc(x, y);
+    """
+    return weave.inline(RCcode, ['x','y'], headers = ["<boost/math/special_functions/ellint_rc.hpp>"])
+
+def InvSqrtQuartic(r1, r2, r3, r4, a, b=None):
+    if b == None:
+        U12 = math.sqrt((a - r1)*(a - r2)) + math.sqrt((a -r3)*(a -r4))
+    else:
+        U12 = (math.sqrt((b-r1)*(b-r2)*(a-r3)*(a-r4)) + math.sqrt((b-r4)*(b-r3)*(a-r2)*(a-r1)))/(b-a)
+
+    U12squared = U12**2
+    return 2*RF(U12squared, U12squared - (r4 - r1)*(r3 - r2), U12squared - (r3 - r1)*(r4 - r2))
+
+def TerribleIntegral(r1,r2,r3,r4,r5, a, b):
+    U12 = (math.sqrt((b-r1)*(b-r2)*(a-r3)*(a-r4)) + math.sqrt((b-r4)*(b-r3)*(a-r2)*(a-r1)))/(b-a)
+    U12sqr = U12**2
+    U13sqr = U12sqr - (r4 - r1)*(r3 - r2)
+    U14sqr = U12sqr - (r3 - r1)*(r4 - r2)
+    Wsqr = U12sqr - (r3 - r1)*(r4 - r1)*(r5 - r2)/(r5 - r1)
+    Qsqr = ((a - r5)*(b - r5)/(a - r1)/(b - r1))*Wsqr
+    Psqr = Qsqr + (r5 - r2)*(r5 - r3)*(r5 - r4)/(r5 - r1)
+    I3 = 2*(r2 - r1)*(r3 - r1)*(r4 - r1)/3.0/(r5-r1) * BoostRJ(U12sqr, U13sqr, U14sqr, Wsqr) + 2*BoostRC(Psqr, Qsqr)
+    I1 = 2*BoostRF(U12sqr, U13sqr, U14sqr)
+
+    return (I3 - I1)/(r5-r1)
--- Equatorial/KerrEqOrbital.py
+++ Equatorial/KerrEqOrbital.py
@@ -6,16 +6,6 @@ import time
 
 pi=np.pi
 
-E, a, L, C = 1.1, 0.99, -5.93, 0
-
-R_coeffs = (E**2-1,2,-a**2+E**2*a**2-L**2-C,2*E**2*a**2-4*E*a*L+2*L**2+2*C,-a**2*C)
-
-roots = np.sort(np.roots(R_coeffs))
-print roots
-#def lmin(e, a):
-#    R_coeffs = (E**2-1,2,-a**2+E**2*a**2-L**2-C,2*E**2*a**2-4*E*a*L+2*L**2+2*C,-a**2*C)
-#    return 
-
 def deflection(e, b):
     r3 = roots[-1]
 #    m = r3*(r2-r1)/(r2*(r3-r1))
@@ -29,17 +19,23 @@ def R(r):
 #    print np.polyval(R_coeffs, r)
     return np.polyval(R_coeffs, r)
 
-def dr_dphi(r, phi):
+def dr_dphi(phi, r):
     sqrt_R = math.sqrt(np.abs(R(r)))
     return -sqrt_R/((L - a*E) + a*(sqrt_R - E*(r**2 + a**2) + L*a)/(r**2 + 2*r + a**2))
 
+############################################################################3
 
-phi = np.linspace(0, (deflection(E, L/math.sqrt(E**2-1)) + np.pi)/2, 1000)
-phi2 = np.linspace(0, deflection(E,L/math.sqrt(E**2-1)) + np.pi, 1999)
-r = integrate.odeint(dr_dphi, 1000, phi, tcrit=phi[-1]).T[0]
+E, a, L, C = 1.1, 0.99, 6, -7
 
-print roots[-1] - r[-1]
-R = np.hstack((r, r[-1:0:-1]))
-x, y = R*np.cos(phi2), R*np.sin(phi2)
-np.savetxt("traj.dat", np.array([x,y]).T)
+R_coeffs = (E**2-1,2,-a**2+E**2*a**2-L**2-C,2*E**2*a**2-4*E*a*L+2*L**2+2*C,-a**2*C)
+
+roots = np.sort(np.roots(R_coeffs))
+
+#phi = np.linspace(0, (deflection(E, L/math.sqrt(E**2-1)) + np.pi), 1999)
+r = 2*roots[-1] + 1.0/np.logspace(-3, 3, 1000)
+
+
+
+#x, y = r*np.cos(phi), r*np.sin(phi)
+#np.savetxt("traj.dat", np.array([x,y]).T)
 
