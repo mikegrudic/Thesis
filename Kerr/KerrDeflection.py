0c108c5758844ecb3eee396946227a14c9b7bcdd
Michael Grudich <mgrudich@mun.ca>
2013-12-11 13:29:48 -0330

    Coordinate tilting for Schw relegated to a function. Weak deflection limit implemented.

--- Kerr/KerrDeflection.py
+++ Kerr/KerrDeflection.py
@@ -61,6 +61,22 @@ def SchwarzDeflection(E, b):
     result[np.invert(fall_in)] = 4*b[np.invert(fall_in)]*ellipf
     return result
 
+def WeakDeflection(E, b):
+#    result = np.zeros(b.shape)
+#    result.fill(pi)
+
+    e2 = E**2 - 1.0
+    wd_coeffs = np.array([pi*(255255/256. + 1155/(4.*e2**3) + 45045/(32.*e2**2) + 135135/(64.*e2)),
+                        716.8 + 2/(5.*e2**5) - 4/e2**4 + 64/e2**3 + 640/e2**2 + 1280/e2,
+                        pi*(3465/64. + 105/(4.*e2**2) + 315/(4.*e2)),
+                        42.666666666666664 - 2/(3.*e2**3) + 8/e2**2 + 48/e2,
+                        pi*(15/4. + 3/e2),
+                        4.0 + 2.0/e2**2,
+                        pi
+                        ])
+
+    return np.polyval(wd_coeffs, 1.0/b)
+
 def EquatorialDeflection(a, E, b, roots):
     C1 = math.sqrt(1-a**2)
     L = b*math.sqrt(E**2-1)
@@ -72,7 +88,15 @@ def EquatorialDeflection(a, E, b, roots):
     part1 = (L - a*E)/math.sqrt(E**2 - 1) * int1
     part3 = a*E/math.sqrt(E**2 - 1) * (int1 + (a**2 -a*L/E + rplus**2)/(rplus - rminus)*int2  - (a**2 -a*L/E + rminus**2)/(rplus - rminus)*int3)
     phi_result = 2*(part1 + part3)
-    return phi_result%(2*np.pi)
+    return phi_result%(2*pi)
+
+def TiltCoords(deflection, theta, bx, by):
+    t1 = np.arctan2(by,bx)
+    y = np.cos(t1)*np.sin(deflection)
+    x = np.cos(deflection)*math.sin(theta) - np.sin(deflection)*math.cos(theta)*np.sin(t1)
+    phi_result = np.arctan2(y, x)
+    theta_result = np.arccos(np.cos(deflection)*math.cos(theta)+math.sin(theta)*np.sin(deflection)*np.sin(t1))
+    return phi_result, theta_result
 
 def KerrDeflection(a, theta, E, bx, by):
     phi_result = np.empty(bx.shape)
@@ -89,24 +113,19 @@ def KerrDeflection(a, theta, E, bx, by):
     #Schwarzschild case
     if a==0.0:
         sch_def = SchwarzDeflection(E, np.sqrt(bx**2 + by**2))
-        t1 = np.arctan2(by,bx)
-        y = np.sin(t1)*np.sin(sch_def)
-        x = np.cos(sch_def)*math.sin(theta) - np.sin(sch_def)*math.sin(theta)*np.sin(t1)
-        phi_result = np.arctan2(y, x)
-        theta_result = np.arccos(np.cos(sch_def)*math.cos(theta)+math.sin(theta)*np.sin(sch_def)*np.sin(t1))
-        return phi_result%(2*np.pi), theta_result%(np.pi)
+        phi_result, theta_result = TiltCoords(sch_def, theta, bx, by)
+        return phi_result%(2*pi), theta_result%(pi)
 
-    s_mu = np.zeros(bx.shape, dtype=np.int64)
+    s_mu = np.empty(bx.shape)
     if -1 < mu0 < 1:
-        q1 = bx**2*math.cos(theta)
         s_mu[by > 0] = 1
         s_mu[by < 0] = -1
         s_mu[(by == 0)*(math.cos(theta)>0)] = -1
         s_mu[(by == 0)*(math.cos(theta)<0)] = 1
     elif mu0 == 1:
-        s_mu = -np.ones(by.shape).astype(int)
+        s_mu = -1
     else:
-        s_mu = np.ones(by.shape).astype(int)
+        s_mu = 1
 
     #solve r quartic
     r_coeffs = np.array([C1*ones, 2*ones, a**2*C1 - L**2 - Q, 2*((-(a*E) + L)**2 + Q), -a**2*Q])
@@ -127,13 +146,12 @@ def KerrDeflection(a, theta, E, bx, by):
     
     L, bx, by, r_roots, zeros, ones, s_mu = L[doesnt_fall_in], bx[doesnt_fall_in], by[doesnt_fall_in], r_roots[doesnt_fall_in].real, zeros[doesnt_fall_in], ones[doesnt_fall_in], s_mu[doesnt_fall_in]
 
-#    print r_roots
     r1, r2, r3, r4 = r_roots.T
     
     #Equatorial case is easy
-    if mu0-math.cos(np.pi/2) == 0.0 and np.all(by==0.0):
+    if mu0-math.cos(pi/2) == 0.0 and np.all(by==0.0):
         phi_result[doesnt_fall_in] = EquatorialDeflection(a, E, bx, (r1,r2,r3,r4))
-        return phi_result%(2*np.pi), ones*np.pi/2
+        return phi_result%(2*pi), ones*pi/2
     
     #mu biquadratic
     discriminant = np.sqrt((bx**2 + by**2)**2 + 2*a**2*(bx - by)*(bx + by)*(-1 + mu0**2) + a**4*(-1 + mu0**2)**2)
@@ -147,7 +165,6 @@ def KerrDeflection(a, theta, E, bx, by):
     mu_max = np.sqrt(M2)
     mu_min = -np.sqrt(M2)
     kSqr = M2/(M2 - M1)
-    xSqr = np.abs(1 - mu0**2/M2)
     n = M2/(1-M2)
 
 #do integrals
@@ -200,4 +217,98 @@ def KerrDeflection(a, theta, E, bx, by):
     phi_result[doesnt_fall_in] = phi
     mu_result[doesnt_fall_in] = mu_final
 
-    return phi_result%(2*np.pi), np.arccos(mu_result)%(np.pi)
+    return phi_result%(2*pi), np.arccos(mu_result)%(pi)
+
+def KerrTrajectory(a, theta, E, bx, by, N):
+    mu0 = math.cos(theta)
+    C1 = E**2 - 1
+    C2 = math.sqrt(1-a**2)
+    L = bx*math.sqrt(C1)*math.sin(theta)
+    Q = (E**2 - 1)*((bx**2 - a**2)*mu0**2 + by**2)    
+
+    if -1 < mu0 < 1:
+        if by==0:
+            if mu0 > 0:
+                s_mu = -1
+            else:
+                s_mu = 1
+        elif by > 0:
+            s_mu = 1
+        else:
+            s_mu = -1
+    else:
+        raise Exception( "Polar orbits not implemented.")
+
+    r_coeffs = np.array([C1, 2, a**2*C1 - L**2 - Q, 2*((-(a*E) + L)**2 + Q), -a**2*Q])    
+    r1, r2, r3, r4 = r_roots = np.sort(np.roots(r_coeffs))
+
+    if np.sum(r_roots.imag) > 0.0 or np.max(r_roots.real) < 1+C2:
+        raise Exception( "Capture orbits not implemented.")
+
+    discriminant = np.sqrt((bx**2 + by**2)**2 + 2*a**2*(bx - by)*(bx + by)*(-1 + mu0**2) + a**4*(-1 + mu0**2)**2)
+    A = -a**2
+    B = a**2 * (1 + mu0**2) - bx**2 - by**2
+    C = by**2 + (bx**2 - a**2)*mu0**2
+    q = -0.5*(B + np.sign(B)*np.sqrt(B**2 - 4*A*C))
+    M1, M2 = np.sort((q/A, C/q),axis = 0)
+    aSqrM2 = (-bx**2 - by**2 + discriminant + a**2*(1+mu0**2))/2
+    aSqrM1 = (-bx**2 - by**2 - discriminant + a**2*(1+mu0**2))/2
+    mu_max = np.sqrt(M2)
+    mu_min = -np.sqrt(M2)
+    kSqr = M2/(M2 - M1)
+    n = M2/(1-M2)    
+
+    r_full = 2*InvSqrtQuartic(r1, r2, r3, r4, r4)
+    r_integral = np.linspace(0, r_full, N)
+    zeros = np.zeros(N)
+    ones = np.ones(N)
+    
+    mu_complete_integral = 2*CarlsonR.BoostRF(0.0, (bx**2+by**2+discriminant - a**2*(1+mu0**2))/2.0, discriminant)
+
+    if np.abs(np.abs(mu0) - mu_max) > 1e-16:
+        mu_initial_integral = (mu_complete_integral/2 - 1/np.sqrt(-M1*M2)*math.fabs(mu0)*CarlsonR.RF((M2-mu0**2)/M2, (M1-mu0**2)/M1, ones)/a)
+    else:
+        mu_initial_integral = mu_complete_integral
+
+    if np.sign(by)*np.sign(mu0) == -1:
+        mu_initial_integral = mu_complete_integral - mu_initial_integral
+
+    N = np.floor((r_integral - mu_initial_integral)/mu_complete_integral)
+
+    integral_remainder = r_integral - N*mu_complete_integral - mu_initial_integral
+    
+    alpha = s_mu*(-1)**N
+
+    J = np.sqrt(M2-M1)*integral_remainder*a
+    mu_final = mu_max*CarlsonR.JacobiCN(J, np.sqrt(kSqr))*alpha
+
+    print np.arccos(mu_final)
+    
+# Do mu-integrals for phi deflection
+#    xSqr_init = 1 - mu0**2/M2
+#    xSqr_final = 1 - mu_final**2/M2
+
+#    P = 1/np.sqrt(M2 - M1)/(1-M2)
+
+#    pi_complete = P*2*CarlsonR.LegendrePiComplete(-n, kSqr)
+#    pi_init = P*CarlsonR.LegendrePi(-n, xSqr_init, kSqr)
+#    pi_final = P*CarlsonR.LegendrePi(-n, xSqr_final, kSqr)
+    
+#    if mu0>0:
+#        pi_init[s_mu==-1] = pi_complete[s_mu==-1] - pi_init[s_mu==-1]
+#    else:
+#        pi_init[s_mu==1] = pi_complete[s_mu==1] - pi_init[s_mu==1]        
+
+#    A = integral_remainder > mu_complete_integral/2
+#    pi_final[A] = pi_complete[A] - pi_final[A]
+    
+#    mu_phi_integral = ((pi_init + pi_final + N*pi_complete)*L/a - a*E*r_integral)/np.sqrt(C1)
+    
+#    r_phi_integral = PhiTerribleIntegral(r1, r2, r3, r4, a, E, L)
+
+#    phi = mu_phi_integral + r_phi_integral
+    
+#    phi_result[doesnt_fall_in] = phi
+#    mu_result[doesnt_fall_in] = mu_final
+
+#    return phi_result%(2*pi), np.arccos(mu_result)%(pi)
