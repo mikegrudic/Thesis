276616027bc6300e4de79736dd51a807fd134618
Mike Grudich <mgrudich@mun.ca>
2014-01-07 19:13:54 -0330

    derp

--- Kerr/KerrDeflection.py
+++ Kerr/KerrDeflection.py
@@ -380,7 +380,7 @@ def KerrDeflectionC(a, theta, E, bx, by):
 
     code = """
     #pragma omp parallel for
-    for (int i = 0; i < nn; i++)
+    for (int i = 0; i < Nbx[0]; i++)
     {
         KerrDeflection(a, E, theta, bx[i], by[i], theta_result[i], phi_result[i]);
     }
